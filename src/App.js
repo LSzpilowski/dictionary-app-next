f4fb1ea2c6e14a3dda30332fb8d7d28a1f743f48
LSzpilowski <lszpilowski@gmail.com>
2023-04-26 10:30:15 +0200

    Import axios & make a first dictionary API calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/" className="btn btn-primary shadow">Hello</a>
       </header>
+      <Dictionary />
+      <footer className="App-footer">Coded by Lukasz Szpilowski</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary () {
+
+let [keyWord, setKeyWord] = useState("");
+
+
+function handleResponse (response) {
+  console.log(response.data[0]);
+}
+
+
+function search (event) {
+  event.preventDefault();
+  alert (`Searching for "${keyWord}" definition`);
+
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+  axios.get(apiUrl).then(handleResponse);
+}
+
+
+
+
+
+
+function changeSearch(event) {
+  setKeyWord(event.target.value)
+}
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={changeSearch} />
+      </form>
+    </div>
+  )
+}
